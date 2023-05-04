10347e57fc7c734c4fef968b4ef9415cab51e30c
facebooker212 <facebooker3124@yahoo.com>
2023-05-03 21:12:55 -0600

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import ThreeSixty from 'react-360-view';
 
+const basePath = "https://fastly-production.24c.in/webin/360";
+//const basePath = '/img/'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ThreeSixty
+        amount={75}
+        imagePath={basePath}
+        fileName="output_{index}.jpeg"
+        spinReverse
+      />
     </div>
   );
 }
